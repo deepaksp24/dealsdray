3a9723968acd5fec61fda358df97e6e2b911b439
deepaksp24 <96986851+deepaksp24@users.noreply.github.com>
2024-05-05 23:14:58 +0530

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'dart:convert';
 import 'package:dealsdray/otpVerify.dart';
 import 'package:dealsdray/service/respAPI.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
-import 'package:http/http.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+//import 'package:flutter/services.dart' show rootBundle;
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
@@ -30,15 +32,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -46,120 +39,133 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3))
+        .then((value) => {FlutterNativeSplash.remove()});
+  }
+
   int _counter = 0;
   var _phoneNumber;
-
-  void _incrementCounter() {
-    setState(() {});
-  }
 
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: // Generated code for this Column Widget...
-            Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            const Opacity(
-              opacity: 0.5,
-              child: Align(
-                alignment: AlignmentDirectional(0, 0),
-                child: Opacity(
-                  opacity: .5,
-                  child: SizedBox(
-                    height: 200,
-                    child: Image(
-                      image: AssetImage(
-                        'assets/images/logo.png',
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back, color: Colors.black),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ),
+          body: // Generated code for this Column Widget...
+              Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              const Opacity(
+                opacity: 0.5,
+                child: Align(
+                  alignment: AlignmentDirectional(0, 0),
+                  child: Opacity(
+                    opacity: .5,
+                    child: SizedBox(
+                      height: 200,
+                      child: Image(
+                        image: AssetImage(
+                          'assets/images/logo.png',
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Switch.adaptive(
-              value: true,
-              onChanged: (newValue) async {},
-            ),
-            const Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [],
-            ),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.all(24),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    const Text(
-                      'Hey good to see you!',
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                    ),
-                    const Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(0, 15, 0, 0),
-                      child: Text(
-                        'Please provide your phone number',
+              Switch.adaptive(
+                value: true,
+                onChanged: (newValue) async {},
+              ),
+              const Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [],
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(24),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const Text(
+                        'Hey good to see you!',
+                        style: TextStyle(
+                            fontSize: 25, fontWeight: FontWeight.bold),
                       ),
-                    ),
-                    Form(
-                      key:
-                          _formKey, // Associate the form key with this Form widget
-                      child: Padding(
-                        padding: EdgeInsets.all(16.0),
-                        child: Column(
-                          children: <Widget>[
-                            TextFormField(
-                              decoration: const InputDecoration(
-                                  hintText: "enter the phone number"),
-                              validator: (value) {
-                                if (value!.isEmpty) {
-                                  return 'Please enter your name.';
-                                }
-                                return null;
-                              },
-                              onSaved: (value) {
-                                _phoneNumber = value!; // Save the entered name
-                              },
-                            ),
-                            const SizedBox(height: 20.0),
-                            ElevatedButton(
-                              onPressed: () async {
-                                if (_formKey.currentState!.validate()) {
-                                  _formKey.currentState!
-                                      .save(); // Save the form data
-                                  print(_phoneNumber);
-                                  if (await login(_phoneNumber)) {
-                                    _navigateToNextPage();
-                                  }
-                                  _navigateToNextPage(); // Print the entered name
-                                } else {
-                                  print('Please fill in the required fields');
-                                }
-                              },
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.red,
-                              ),
-                              child: const Text(
-                                'Send code',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                            ),
-                          ],
+                      const Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 15, 0, 0),
+                        child: Text(
+                          'Please provide your phone number',
                         ),
                       ),
-                    ),
-                  ],
+                      Form(
+                        key:
+                            _formKey, // Associate the form key with this Form widget
+                        child: Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Column(
+                            children: <Widget>[
+                              TextFormField(
+                                decoration: const InputDecoration(
+                                    hintText: "enter the phone number"),
+                                validator: (value) {
+                                  if (value!.isEmpty) {
+                                    return 'Please enter your name.';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (value) {
+                                  _phoneNumber =
+                                      value!; // Save the entered name
+                                },
+                              ),
+                              const SizedBox(height: 20.0),
+                              ElevatedButton(
+                                onPressed: () async {
+                                  if (_formKey.currentState!.validate()) {
+                                    _formKey.currentState!
+                                        .save(); // Save the form data
+                                    print(_phoneNumber);
+                                    if (await login(_phoneNumber)) {
+                                      _navigateToNextPage();
+                                    }
+                                    _navigateToNextPage(); // Print the entered name
+                                  } else {
+                                    print('Please fill in the required fields');
+                                  }
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.red,
+                                ),
+                                child: const Text(
+                                  'Send code',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
-        ));
+            ],
+          )),
+    );
   }
 
   void _navigateToNextPage() {
